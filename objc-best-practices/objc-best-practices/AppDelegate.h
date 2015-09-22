40eaef42371ad3360034ff1540221d1899610024
jiakailian <jiakai.lian@gmail.com>
2015-09-22 22:17:28 +1000

    initial commit for the project

---
+++ objc-best-practices/objc-best-practices/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  objc-best-practices
+//
+//  Created by jiakai lian on 22/09/2015.
+//  Copyright (c) 2015 jiakai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ objc-best-practices/objc-best-practices/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  objc-best-practices
+//
+//  Created by jiakai lian on 22/09/2015.
+//  Copyright (c) 2015 jiakai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
